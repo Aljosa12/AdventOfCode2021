22699af85169355d677ff889d34827ba5af35116
Aljosa <lgaljo@gmail.com>
2021-12-03 15:43:03 +0100

    Init

---
+++ src/advent-03/part1.js
@@ -0,0 +1,28 @@
+import { inputs, qwert } from './inputs.js';
+
+let counter = 0;
+
+function calc(input) {
+    let gamma = '';
+    let epsilon = '';
+    for (let i = 0; i < input[0].length; i++) {
+        let one = 0,
+            zero = 0;
+        for (let k = 0; k < input.length; k++) {
+            // console.log(input[k][i], k);
+            input[k][i] == 0 ? zero++ : one++;
+            counter++;
+        }
+        zero > one ? (gamma += '0') : (gamma += '1');
+    }
+
+    for (let i = 0; i < gamma.length; i++) {
+        gamma[i] === '1' ? (epsilon += '0') : (epsilon += '1');
+    }
+
+    console.log(gamma, epsilon);
+}
+
+calc(inputs);
+
+// res 1916 2179
---
+++ src/advent-03/part2.js
@@ -0,0 +1,52 @@
+import { inputs } from './inputs.js';
+let inp = [];
+for (let i = 0; i < inputs.length; i++) {
+    inp.push(inputs[i]);
+}
+
+function calcGamma(input, num, air) {
+    let res = '';
+
+    for (let i = num; i < num + 1; i++) {
+        let one = 0,
+            zero = 0;
+        for (let k = 0; k < input.length; k++) {
+            input[k][i] == 0 ? zero++ : one++;
+        }
+
+        if (air === 'oxy') {
+            zero > one ? (res += '0') : (res += '1');
+        } else {
+            zero <= one ? (res += '0') : (res += '1');
+        }
+    }
+
+    return res;
+}
+
+function oxygenGenerator(inputs, air) {
+    let input = inputs;
+    for (let i = 0; i < input[0].length; i++) {
+        let mostCommonOxy = calcGamma(input, i, air);
+
+        for (let k = 0; k < input.length; k++) {
+            if (input[k][i] !== mostCommonOxy) {
+                input.splice(k, 1);
+                k--;
+            }
+        }
+
+        if (input.length === 1) {
+            break;
+        }
+    }
+
+    return input;
+}
+
+const oxy = oxygenGenerator(inputs, 'oxy');
+const air = oxygenGenerator(inp, 'air');
+
+let res = parseInt(oxy, 2) * parseInt(air, 2);
+
+console.log(res);
